4f241a3fcd88d49b82d22c827def6f390342b8b3
Matteo <matteo.violani@studio.unibo.it>
2025-10-20 18:00:46 +0200

    chore: added constructor to fake data dostributor

--- app/src/main/java/pcd/ass3/sudoku/mom/FakeDataDistributor.java
+++ app/src/main/java/pcd/ass3/sudoku/mom/FakeDataDistributor.java
@@ -3,13 +3,20 @@
 
 package pcd.ass3.sudoku.mom;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 
-class FakeDataDistributor implements DataDistributor {
+public class FakeDataDistributor implements DataDistributor {
 
   private List<Map<String, String>> boards;
+
+  public FakeDataDistributor() {
+    this.boards = new ArrayList<>();
+    var boardDetails = Map.of("name", "board-1", "author", "teo");
+      this.boards.add(boardDetails);
+  }
 
     @Override
     public void init(SharedDataListener controller) {
@@ -17,38 +24,33 @@ class FakeDataDistributor implements DataDistributor {
 
     @Override
     public void registerBoard(JsonData boarData) {
-      var boardDetails = Map.of("name", "board-1", "author", "teo");
-      this.boards.add(boardDetails);
     }
 
     @Override
     public void joinBoard(String nickname, String boardName) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void leaveBoard() {
-      throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void shareUpdate(JsonData edits) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void updateCursor(JsonData userInfo) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public List<JsonData> existingBoards() {
-      return (List<JsonData>) this.boards.stream().map(e -> new JsonData() {
+      return this.boards.stream().map(e -> 
+      (JsonData) new JsonData() {
         @Override
         public String getJsonString() {
           return  e.toString();
         }
-      });
+      }).toList();
     }
 
 }
