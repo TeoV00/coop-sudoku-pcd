384367c7a90f8145518e9482ef4ebc0d3655b098
Matteo <matteo.violani@studio.unibo.it>
2025-10-24 13:45:30 +0200

    feat: centrale panel for no selected board

---
+++ app/src/main/java/pcd/ass3/sudoku/view/subviews/NoBoardPanel.java
@@ -0,0 +1,18 @@
+package pcd.ass3.sudoku.view.subviews;
+
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class NoBoardPanel extends JPanel {
+
+  public NoBoardPanel() {
+    setLayout(new GridBagLayout());
+    JLabel label = new JLabel("No board joined, before playing join a board!");
+    label.setFont(new Font("Arial", Font.BOLD, 16));
+    add(label, new GridBagConstraints());
+  }
+}
