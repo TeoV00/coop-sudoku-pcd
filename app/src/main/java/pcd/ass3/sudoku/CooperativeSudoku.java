745713f0ebcfa3b587465d9632e359f766147e11
Matteo <matteo.violani@studio.unibo.it>
2025-12-26 13:40:16 +0100

    chore: improved main, selcted which version run

--- app/src/main/java/pcd/ass3/sudoku/CooperativeSudoku.java
+++ app/src/main/java/pcd/ass3/sudoku/CooperativeSudoku.java
@@ -1,6 +1,7 @@
 package pcd.ass3.sudoku;
 
 import pcd.ass3.sudoku.communication.DataDistributor;
+import pcd.ass3.sudoku.communication.mom.StreamRabbitDistributor;
 import pcd.ass3.sudoku.communication.rmi.client.RmiClientDistributor;
 import pcd.ass3.sudoku.controller.ControllerImpl;
 import pcd.ass3.sudoku.view.SudokuBoardUI;
@@ -8,7 +9,14 @@ import pcd.ass3.sudoku.view.SudokuBoardUI;
 public class CooperativeSudoku {
 
     public static void main(String[] args) {
-        DataDistributor distr = new RmiClientDistributor("RmiServer");// new StreamRabbitDistributor();
+        DataDistributor distr;
+        if (args.length > 0 && args[0].equals("rmi")) {
+            distr = new RmiClientDistributor("RmiServer");
+            System.out.println("RMI Version");
+        } else {
+            distr = new StreamRabbitDistributor();
+            System.out.println("Stream RABBITMQ Version");
+        }
         //distr.setHost("192.168.0.2");
         var controller = new ControllerImpl(distr);
         var gui = new SudokuBoardUI(controller);
